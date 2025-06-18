c025311b7aa05fc0e3c06de5607d1102a59a2bf6
aliifz <mnamz101@gmail.com>
2025-06-18 21:44:05 +0800

    test hook

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,7 +13,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        // Today's statistics
+        // Today's statistics -- comment out for now
         $today = Carbon::today();
         $todayStats = [
             'sales' => Order::whereDate('created_at', $today)->sum('total'),
