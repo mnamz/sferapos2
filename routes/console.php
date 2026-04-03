b130cc03ca6ab7b23f451d4b225841cb8f343eba
aliifz <mnamz101@gmail.com>
2026-04-03 16:54:13 +0800

    fix: update sales check daily schedule to run at 23:30

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::command('backup:run')->dailyAt('00:00');
-Schedule::command('sales:check-daily')->dailyAt('00:30')->timezone('Asia/Kuala_Lumpur');
+Schedule::command('sales:check-daily')->dailyAt('23:30')->timezone('Asia/Kuala_Lumpur');
 Schedule::command('myinvois:push-queue')->hourly()->timezone('Asia/Kuala_Lumpur');
